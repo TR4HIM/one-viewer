e640c73d8ca202f1dd8c6eadc24941ee47d41f47
Trahim Soufiane <tr4him@gmail.com>
2019-06-16 18:09:59 +0100

    Add click to button and key binding navigations

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,6 @@
             getLatestPost : function () {
                 // OneViewer API to get the latest post by category
                 let showLatestPostUrl = `${API_ENDPOINT}category/1/latest`;
-                console.log(showLatestPostUrl);
 
                 axios.get(showLatestPostUrl).then(response => {
                     this.results        = response.data;
@@ -31,9 +30,47 @@
                     this.previousPost   = response.data.prevPostId;
                 });
             },
+
+            //Get post by id with on click (next/previous buttons)
+            showPost: function (id) {
+                // OneViewer API to get the latest post by id
+                let PostByIdUrl = `${API_ENDPOINT}getpostbyid/${id}`;
+
+                axios.get(PostByIdUrl).then(response => {
+                    this.results        = response.data;
+                    this.nextPost       = response.data.nextPostId;
+                    this.previousPost   = response.data.prevPostId;
+                })
+            },
+
+            //Next/Previous with keys
+            // j : Previous
+            // k : Next
+            keyNavigation: function (event) {
+                //check if Keycode is for J
+                if (event.keyCode == 74) {
+                    console.log('Previous');
+                    if (this.previousPost != null)
+                        this.showPost(this.previousPost);
+                }
+                
+                //check if Keycode is for K
+                if (event.keyCode == 75) {
+                    console.log('Next');
+                    if (this.nextPost != null)
+                        this.showPost(this.nextPost);
+                }
+            },
         },
 
         mounted() {
+            //save this to use THIS in innerFunctions
+            let _self = this;
+            //Bind keyup to application window
+            window.addEventListener('keyup', function (ev) {
+                _self.keyNavigation(ev);
+            });
+
             this.getLatestPost();
         }
     });
